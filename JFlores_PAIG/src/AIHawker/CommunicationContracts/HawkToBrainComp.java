c0cf0c1e85e3e3cd0b70115b70a155dfaffbe8c1
Narucross <saber_knight_x@yahoo.com>
2013-07-23 14:59:45 -0600

    Started Some code; Hawker is the vendor, interface added to simulate contract between a Personality and its vendor

---
+++ JFlores_PAIG/src/AIHawker/CommunicationContracts/HawkToBrainComp.java
@@ -0,0 +1,26 @@
+package AIHawker.CommunicationContracts;
+
+/**
+ * Basically the initial contract between the AIHawker and an individual AI Brain,
+ * this simply states that a Hawker is able to get a response or can expect a compatible response
+ * from an AI Personality
+ * User: jflores
+ * Date: 7/23/13
+ * Time: 10:24 AM
+ */
+public interface HawkToBrainComp {
+    /**
+     * Every kind of personality will have a designated response in the form of IDK(I don't know)
+     * This method allows the Hawker can use this method and an equals method to the response to determine
+     * if the AI didn't have a response available for the User.
+     * @return returns the IDK response of the Personality
+     */
+    public String getIDKReply();
+
+    /**
+     * Send a conventional grammar into this method and receive a response from the AI Personality
+     * @param conventional_Grammar to be determined
+     * @return
+     */
+    public String requestResponce(String conventional_Grammar);
+}//end of interface
---
+++ JFlores_PAIG/src/AIHawker/Hawker.java
@@ -0,0 +1,14 @@
+package AIHawker;
+
+/**
+ * The general Idea of what all AI Hawker's are suppose to do.
+ * Each one should:
+ * Serve as the interface between itself and the AI
+ * User: jflores
+ * Date: 7/23/13
+ * Time: 10:40 AM
+ */
+public interface Hawker //implements FloresMenu???
+{
+
+}//end of interface
